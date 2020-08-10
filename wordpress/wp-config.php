571d13d0b0d6f4dbea9578f108c25f8ce6993e04
Yago Ésquines <yago.almeida@4linux.com.br>
2020-08-09 21:33:36 -0300

    Inclusão do FS_METHOD

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -36,6 +36,7 @@ define( 'DB_CHARSET', 'utf8' );
 
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
+define('FS_METHOD','direct');
 
 /**#@+
  * Authentication Unique Keys and Salts.
